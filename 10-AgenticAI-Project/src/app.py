880fe175090ffad554e762b89c9370ad19586e4c
AdilHayat21173 <hayatadil300@gmail.com>
2025-07-30 04:29:43 -0700

    add agenticai project

---
+++ 10-AgenticAI-Project/src/app.py
@@ -0,0 +1,11 @@
+from langgraphagenticai.main import load_langgraph_agenticai_app
+
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+# call the function to run the application
+
+if __name__ == "__main__":
+    load_langgraph_agenticai_app()
+
